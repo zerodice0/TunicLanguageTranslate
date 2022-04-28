1da835cde13828b2717077136e2e435a5bc23250
zerodice0 <zerodice0@gmail.com>
2022-04-28 14:27:53 +0900

    refactor: move to calculate logic to model - add Language interface - add LanguageComponent interface - add calculateColumns function -- Columns를 계산해주는 함수로, 중복된 로직을 제거하기 위해 별도 함수로 분리 - add calculateRows function -- Rows를 계산해주는 함수로, 중복된 로직을 제거하기 위해 별도 함수로 분리 - add calculateConsonantsAndVowels function -- consonants와 vowels를 만들어주기 위한 함수

--- src/components/LanguageInputCanvas/LanguageInputCanvas.model.tsx
+++ src/components/LanguageInputCanvas/LanguageInputCanvas.model.tsx
@@ -1,6 +1,128 @@
+import { Line } from "../..//modules/Drawer/Drawer.model";
+
 export interface LanguageInputCanvasProps {
   width: number;
   height: number;
   rowCount?: number;
   columnCount?: number;
 }
+
+interface Language {
+  vowels: LanguageComponent;
+  consonants: LanguageComponent;
+}
+interface LanguageComponent {
+  upSide: Line[];
+  downSide: Line[];
+}
+
+export const calculateColumns = (canvasHeight: number, columnCount: number) =>
+  Array(columnCount)
+    .fill(0)
+    .map((_, index) => (index * canvasHeight) / columnCount)
+    .slice(1);
+export const calculateRows = (canvasWidth: number, rowCount: number) =>
+  Array(rowCount)
+    .fill(0)
+    .map((_, index) => (index * canvasWidth) / rowCount)
+    .slice(1);
+
+export const calculateConsonantsAndVowels = (
+  canvasWidth: number,
+  canvasHeight: number,
+  columnCount: number,
+  rowCount: number,
+  rows?: number[],
+  columns?: number[]
+): Language => {
+  const _rows = rows ?? calculateRows(canvasWidth, rowCount);
+  const _columns = columns ?? calculateColumns(canvasHeight, columnCount);
+
+  const columnsHeightHalf = canvasHeight / columnCount / 2;
+
+  const point0 = { x: _rows[0], y: _columns[1] };
+  const point1 = { x: _rows[2], y: _columns[0] };
+  const point2 = { x: _rows[4], y: _columns[1] };
+  const point3 = {
+    x: _rows[2],
+    y: _columns[3] - columnsHeightHalf,
+  };
+  const point4 = {
+    x: _rows[2],
+    y: canvasHeight / 2,
+  };
+  const point5 = {
+    x: _rows[2],
+    y: _columns[4] - columnsHeightHalf,
+  };
+  const point6 = { x: _rows[0], y: _columns[5] };
+  const point7 = { x: _rows[2], y: _columns[6] };
+  const point8 = { x: _rows[4], y: _columns[5] };
+
+  return {
+    vowels: {
+      upSide: [
+        {
+          start: point0,
+          end: { x: _rows[0], y: _columns[3] },
+        },
+        {
+          start: point0,
+          end: point1,
+        },
+        {
+          start: point1,
+          end: point2,
+        },
+      ],
+      downSide: [
+        {
+          start: { x: _rows[0], y: _columns[4] },
+          end: point6,
+        },
+        {
+          start: point6,
+          end: point7,
+        },
+        {
+          start: point7,
+          end: point8,
+        },
+      ],
+    },
+    consonants: {
+      upSide: [
+        {
+          start: point0,
+          end: point3,
+        },
+        {
+          start: point1,
+          end: point3,
+        },
+        {
+          start: point2,
+          end: point3,
+        },
+        {
+          start: point4,
+          end: point3,
+        },
+      ],
+      downSide: [
+        {
+          start: point5,
+          end: point6,
+        },
+        {
+          start: point5,
+          end: point7,
+        },
+        {
+          start: point5,
+          end: point8,
+        },
+      ],
+    },
+  };
+};
--- src/components/LanguageInputCanvas/LanguageInputCanvas.tsx
+++ src/components/LanguageInputCanvas/LanguageInputCanvas.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 import { drawLine, drawLines, drawPoints } from "../../modules/Drawer/Drawer";
-import { LanguageInputCanvasProps } from "./LanguageInputCanvas.model";
+import {
+  calculateColumns,
+  calculateConsonantsAndVowels,
+  calculateRows,
+  LanguageInputCanvasProps,
+} from "./LanguageInputCanvas.model";
 
 const Canvas = styled.canvas<LanguageInputCanvasProps>`
   <canvas width=${prop => prop.width} height=${prop => prop.height}/>
@@ -15,14 +20,17 @@ export const LanguageInputCanvas = ({
   rowCount = 6,
 }: LanguageInputCanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const rows = Array(rowCount)
-    .fill(0)
-    .map((_, index) => (index * width) / rowCount)
-    .slice(1);
-  const columns = Array(columnCount)
-    .fill(0)
-    .map((_, index) => (index * height) / columnCount)
-    .slice(1);
+
+  const rows = calculateRows(width, rowCount);
+  const columns = calculateColumns(height, columnCount);
+  const { consonants, vowels } = calculateConsonantsAndVowels(
+    width,
+    height,
+    columnCount,
+    rowCount,
+    rows,
+    columns
+  );
 
   useEffect(() => {
     const context = canvasRef.current?.getContext("2d");
@@ -44,26 +52,35 @@ export const LanguageInputCanvas = ({
       );
 
       // Draw Crossline
-      drawLine(context, {
-        start: { x: 0, y: height / 2 },
-        end: { x: width, y: height / 2 },
-      });
+      drawLine(
+        context,
+        {
+          start: { x: 0, y: height / 2 },
+          end: { x: width, y: height / 2 },
+        },
+        { lineWidth: 5 }
+      );
 
       // Draw Base Points
-      drawPoints(
-        context,
-        [
-          { x: rows[0], y: columns[1] },
-          { x: rows[2], y: columns[0] },
-          { x: rows[4], y: columns[1] },
-          { x: rows[0], y: columns[5] },
-          { x: rows[2], y: columns[6] },
-          { x: rows[4], y: columns[5] },
-          { x: rows[2], y: columns[2] + height / columnCount / 2 },
-          { x: rows[2], y: columns[4] - height / columnCount / 2 },
-        ],
-        { globalAlpha: 0.3 }
-      );
+      // drawPoints(
+      //   context,
+      //   [
+      //     { x: rows[0], y: columns[1] },
+      //     { x: rows[2], y: columns[0] },
+      //     { x: rows[4], y: columns[1] },
+      //     point5,
+      //     { x: rows[2], y: columns[6] },
+      //     { x: rows[4], y: columns[5] },
+      //     { x: rows[2], y: columns[2] + height / columnCount / 2 },
+      //     { x: rows[2], y: columns[4] - height / columnCount / 2 },
+      //   ],
+      //   { globalAlpha: 0.3 }
+      // );
+
+      drawLines(context, consonants.upSide, { lineWidth: 5 });
+      drawLines(context, consonants.downSide, { lineWidth: 5 });
+      drawLines(context, vowels.upSide, { lineWidth: 5 });
+      drawLines(context, vowels.downSide, { lineWidth: 5 });
     }
   }, []);
 
