28e25c0bae5fa24e6b6f15c996d02b1bc5761835
zerodice0 <zerodice0@gmail.com>
2022-04-28 17:06:57 +0900

    refactor: change point0~point8 to array

--- src/components/LanguageInputCanvas/LanguageInputCanvas.model.tsx
+++ src/components/LanguageInputCanvas/LanguageInputCanvas.model.tsx
@@ -40,87 +40,89 @@ export const calculateConsonantsAndVowels = (
 
   const columnsHeightHalf = canvasHeight / columnCount / 2;
 
-  const point0 = { x: _rows[0], y: _columns[1] };
-  const point1 = { x: _rows[2], y: _columns[0] };
-  const point2 = { x: _rows[4], y: _columns[1] };
-  const point3 = {
-    x: _rows[2],
-    y: _columns[3] - columnsHeightHalf,
-  };
-  const point4 = {
-    x: _rows[2],
-    y: canvasHeight / 2,
-  };
-  const point5 = {
-    x: _rows[2],
-    y: _columns[4] - columnsHeightHalf,
-  };
-  const point6 = { x: _rows[0], y: _columns[5] };
-  const point7 = { x: _rows[2], y: _columns[6] };
-  const point8 = { x: _rows[4], y: _columns[5] };
+  const points = [
+    { x: _rows[0], y: _columns[1] },
+    { x: _rows[2], y: _columns[0] },
+    { x: _rows[4], y: _columns[1] },
+    {
+      x: _rows[2],
+      y: _columns[3] - columnsHeightHalf,
+    },
+    {
+      x: _rows[2],
+      y: canvasHeight / 2,
+    },
+    {
+      x: _rows[2],
+      y: _columns[4] - columnsHeightHalf,
+    },
+    { x: _rows[0], y: _columns[5] },
+    { x: _rows[2], y: _columns[6] },
+    { x: _rows[4], y: _columns[5] },
+  ];
 
   return {
     vowels: {
       upSide: [
         {
-          start: point0,
+          start: points[0],
           end: { x: _rows[0], y: _columns[3] },
         },
         {
-          start: point0,
-          end: point1,
+          start: points[0],
+          end: points[1],
         },
         {
-          start: point1,
-          end: point2,
+          start: points[1],
+          end: points[2],
         },
       ],
       downSide: [
         {
           start: { x: _rows[0], y: _columns[4] },
-          end: point6,
+          end: points[6],
         },
         {
-          start: point6,
-          end: point7,
+          start: points[6],
+          end: points[7],
         },
         {
-          start: point7,
-          end: point8,
+          start: points[7],
+          end: points[8],
         },
       ],
     },
     consonants: {
       upSide: [
         {
-          start: point0,
-          end: point3,
+          start: points[0],
+          end: points[3],
         },
         {
-          start: point1,
-          end: point3,
+          start: points[1],
+          end: points[3],
         },
         {
-          start: point2,
-          end: point3,
+          start: points[2],
+          end: points[3],
         },
         {
-          start: point4,
-          end: point3,
+          start: points[4],
+          end: points[3],
         },
       ],
       downSide: [
         {
-          start: point5,
-          end: point6,
+          start: points[5],
+          end: points[6],
         },
         {
-          start: point5,
-          end: point7,
+          start: points[5],
+          end: points[7],
         },
         {
-          start: point5,
-          end: point8,
+          start: points[5],
+          end: points[8],
         },
       ],
     },
