ebc4ccbdeed632ea4e20d275103fd8fc031e9b10
zerodice0 <zerodice0@gmail.com>
2022-04-29 17:53:06 +0900

    refactor: add test case

--- src/stories/LanguageInputCanvas.stories.tsx
+++ src/stories/LanguageInputCanvas.stories.tsx
@@ -19,3 +19,41 @@ Normal.args = {
   width: 240,
   height: 360,
 };
+
+export const AllConsonants = Template.bind([]);
+AllConsonants.args = {
+  width: 240,
+  height: 360,
+  consonants: [0, 1, 2, 3, 4, 5],
+};
+
+export const AllVowels = Template.bind([]);
+AllVowels.args = {
+  width: 240,
+  height: 360,
+  vowels: [0, 1, 2, 3, 4, 5],
+};
+
+export const There = Template.bind([]);
+There.args = {
+  width: 240,
+  height: 360,
+  consonants: [1, 3, 4, 5],
+  vowels: [0, 3, 5],
+};
+
+export const Li = Template.bind([]);
+Li.args = {
+  width: 240,
+  height: 360,
+  consonants: [1, 4],
+  vowels: [4, 5],
+};
+
+export const A = Template.bind([]);
+A.args = {
+  width: 240,
+  height: 360,
+  consonants: [0, 3, 5],
+  vowels: [0, 1, 2, 3, 4],
+};
