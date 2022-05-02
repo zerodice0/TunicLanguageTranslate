c2f73e1c71fe495fe8932d4f800300870fd93542
zerodice0 <zerodice0@gmail.com>
2022-05-03 01:32:32 +0900

    feat: update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,52 @@
+import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { LanguageInput } from "./components/LanguageInput/LanguageInput";
+import { LanguageInputCanvas } from "./components/LanguageRenderer/LanguageRenderer";
+import {
+  parsingConsonantsToIPA,
+  parsingVowelsToIPA,
+} from "./modules/LanguageParer/LanguageParser";
+
 export default function App() {
-  return null;
+  const [inputLanguage, setInputLanguage] = useState<{
+    consonants: number[];
+    vowels: number[];
+  }>({
+    consonants: [],
+    vowels: [],
+  });
+
+  return (
+    <Box textAlign="center">
+      <Heading>Tunic Language Translator</Heading>
+      <Flex align="center" justify="center">
+        <Flex direction="column" width="100%">
+          <Center width="100%">
+            <Text>Input</Text>
+          </Center>
+          <Center>
+            <LanguageInputCanvas
+              width={100}
+              height={150}
+              isDrawingFrame={true}
+              consonants={inputLanguage.consonants}
+              vowels={inputLanguage.vowels}
+              defaultLineColor="#ffffff"
+              defaultLineWidth={2}
+            />
+          </Center>
+          <Text>
+            Current:
+            {parsingConsonantsToIPA(inputLanguage.consonants) +
+              parsingVowelsToIPA(inputLanguage.vowels)}
+          </Text>
+          <LanguageInput />
+        </Flex>
+        <Center width="100%">
+          <Text>Result</Text>
+        </Center>
+      </Flex>
+    </Box>
+  );
 }
