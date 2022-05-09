542055cf75585d24dc3f8305cb6514e7d5b7bd3a
zerodice0 <zerodice0@gmail.com>
2022-05-09 11:04:11 +0900

    refactor: detach render function to viewModel - add hover properties - move render function to viewModel - add mouseover event handler to canvas - add mouseleave event handler to canvas

--- src/components/LanguageRenderer/LanguageRenderer.tsx
+++ src/components/LanguageRenderer/LanguageRenderer.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useRef } from "react";
+import styled from "styled-components";
 
-import { drawLine, drawLines } from "../../modules/Drawer/Drawer";
 import {
   calculateColumns,
-  calculateConsonantsAndVowels,
   calculateRows,
   LanguageRendererProps,
 } from "./LanguageRenderer.model";
+import { render } from "./LanguageRenderer.viewModel";
+
+const Canvas = styled.canvas`
+  border-radius: 5px;
+`;
 
 export const LanguageRenderer = ({
   width,
@@ -17,104 +21,71 @@ export const LanguageRenderer = ({
   vowels,
   defaultLineColor = "black",
   defaultLineWidth = 1,
+  hoverLineColor = "black",
+  hoverLineWidth = 1,
   isDrawingFrame = false,
 }: LanguageRendererProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const rows = calculateRows(width, rowCount);
   const columns = calculateColumns(height, columnCount);
-  const { consonants: _consonants, vowels: _vowels } =
-    calculateConsonantsAndVowels(
-      width,
-      height,
-      columnCount,
-      rowCount,
-      rows,
-      columns
-    );
+
+  const _render = (
+    canvas: HTMLCanvasElement | null,
+    lineWidth: number,
+    lineColor: string
+  ) => {
+    const context = canvas?.getContext("2d");
+    if (context) {
+      render(
+        context,
+        isDrawingFrame,
+        rows,
+        columns,
+        width,
+        height,
+        lineWidth,
+        lineColor,
+        consonants ?? [],
+        vowels ?? [],
+        columnCount,
+        rowCount
+      );
+    }
+  };
+
+  const _clear = (canvas: HTMLCanvasElement | null) => {
+    const context = canvas?.getContext("2d");
+    if (context) {
+      context.clearRect(0, 0, width, height);
+    }
+  };
 
   useEffect(() => {
-    const context = canvasRef.current?.getContext("2d");
-    if (context) {
-      // Draw Frame
-      isDrawingFrame &&
-        drawLines(
-          context,
-          [
-            ...rows.map(row => ({
-              start: { x: row, y: 0 },
-              end: { x: row, y: height },
-            })),
-            ...columns.map(column => ({
-              start: { x: 0, y: column },
-              end: { x: width, y: column },
-            })),
-          ],
-          {
-            globalAlpha: 0.3,
-            lineColor: defaultLineColor,
-            lineWidth: defaultLineWidth,
-          }
-        );
+    const canvas = canvasRef.current;
 
-      // Draw Crossline
-      drawLine(
-        context,
-        {
-          start: { x: 0, y: height / 2 },
-          end: { x: width, y: height / 2 },
-        },
-        { lineWidth: defaultLineWidth, lineColor: defaultLineColor }
-      );
+    const mouseOver = () => {
+      _clear(canvas);
+      _render(canvas, hoverLineWidth, hoverLineColor);
+    };
+    const mouseLeave = () => {
+      _clear(canvas);
+      _render(canvas, defaultLineWidth, defaultLineColor);
+    };
+    canvasRef.current?.addEventListener("mouseover", mouseOver);
+    canvasRef.current?.addEventListener("mouseleave", mouseLeave);
 
-      // Draw Base Points
-      // drawPoints(
-      //   context,
-      //   [
-      //     { x: rows[0], y: columns[1] },
-      //     { x: rows[2], y: columns[0] },
-      //     { x: rows[4], y: columns[1] },
-      //     point5,
-      //     { x: rows[2], y: columns[6] },
-      //     { x: rows[4], y: columns[5] },
-      //     { x: rows[2], y: columns[2] + height / columnCount / 2 },
-      //     { x: rows[2], y: columns[4] - height / columnCount / 2 },
-      //   ],
-      //   { globalAlpha: 0.3 }
-      // );
+    _render(canvas, defaultLineWidth, defaultLineColor);
 
-      const consonantList = consonants?.map(
-        consonant => [..._consonants.upSide, ..._consonants.downSide][consonant]
-      );
-      const vowelList = vowels?.map(
-        vowel => [..._vowels.upSide, ..._vowels.downSide][vowel]
-      );
-      consonantList &&
-        drawLines(context, consonantList, {
-          lineWidth: defaultLineWidth,
-          lineColor: defaultLineColor,
-        });
-      if (
-        consonants?.some(
-          consonant => consonant === 0 || consonant === 1 || consonant === 2
-        )
-      ) {
-        drawLines(context, _consonants.sub, {
-          lineWidth: defaultLineWidth,
-          lineColor: defaultLineColor,
-        });
-      }
-      vowelList &&
-        drawLines(context, vowelList, {
-          lineWidth: defaultLineWidth,
-          lineColor: defaultLineColor,
-        });
-    }
+    return () => {
+      canvas?.removeEventListener("mouseover", mouseOver);
+      canvas?.removeEventListener("mouseleave", mouseLeave);
+    };
   }, []);
 
   return (
     <div>
-      <canvas ref={canvasRef} width={width} height={height} />
+      <Canvas ref={canvasRef} width={width} height={height} />
     </div>
   );
 };
---
+++ src/components/LanguageRenderer/LanguageRenderer.viewModel.tsx
@@ -0,0 +1,101 @@
+import { drawLine, drawLines } from "../../modules/Drawer/Drawer";
+import { calculateConsonantsAndVowels } from "./LanguageRenderer.model";
+
+export const render = (
+  context: CanvasRenderingContext2D,
+  isDrawingFrame: boolean,
+  rows: number[],
+  columns: number[],
+  width: number,
+  height: number,
+  lineWidth: number,
+  lineColor: string,
+  consonants: number[],
+  vowels: number[],
+  columnCount: number,
+  rowCount: number
+) => {
+  const { consonants: _consonants, vowels: _vowels } =
+    calculateConsonantsAndVowels(
+      width,
+      height,
+      columnCount,
+      rowCount,
+      rows,
+      columns
+    );
+
+  // Draw Frame
+  isDrawingFrame &&
+    drawLines(
+      context,
+      [
+        ...rows.map(row => ({
+          start: { x: row, y: 0 },
+          end: { x: row, y: height },
+        })),
+        ...columns.map(column => ({
+          start: { x: 0, y: column },
+          end: { x: width, y: column },
+        })),
+      ],
+      {
+        globalAlpha: 0.3,
+        lineColor: lineColor,
+        lineWidth: lineWidth,
+      }
+    );
+
+  // Draw Crossline
+  drawLine(
+    context,
+    {
+      start: { x: 0, y: height / 2 },
+      end: { x: width, y: height / 2 },
+    },
+    { lineWidth: lineWidth, lineColor: lineColor }
+  );
+
+  // Draw Base Points
+  // drawPoints(
+  //   context,
+  //   [
+  //     { x: rows[0], y: columns[1] },
+  //     { x: rows[2], y: columns[0] },
+  //     { x: rows[4], y: columns[1] },
+  //     point5,
+  //     { x: rows[2], y: columns[6] },
+  //     { x: rows[4], y: columns[5] },
+  //     { x: rows[2], y: columns[2] + height / columnCount / 2 },
+  //     { x: rows[2], y: columns[4] - height / columnCount / 2 },
+  //   ],
+  //   { globalAlpha: 0.3 }
+  // );
+
+  const consonantList = consonants?.map(
+    consonant => [..._consonants.upSide, ..._consonants.downSide][consonant]
+  );
+  const vowelList = vowels?.map(
+    vowel => [..._vowels.upSide, ..._vowels.downSide][vowel]
+  );
+  consonantList &&
+    drawLines(context, consonantList, {
+      lineWidth: lineWidth,
+      lineColor: lineColor,
+    });
+  if (
+    consonants?.some(
+      consonant => consonant === 0 || consonant === 1 || consonant === 2
+    )
+  ) {
+    drawLines(context, _consonants.sub, {
+      lineWidth: lineWidth,
+      lineColor: lineColor,
+    });
+  }
+  vowelList &&
+    drawLines(context, vowelList, {
+      lineWidth: lineWidth,
+      lineColor: lineColor,
+    });
+};
