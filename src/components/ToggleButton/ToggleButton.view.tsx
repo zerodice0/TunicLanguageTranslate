bb61f4e2096876d0dc3fd92ce26df899f2e17f3b
zerodice0 <zerodice0@gmail.com>
2022-05-11 00:09:36 +0900

    refactor: remove pseudo selector
    - hover/checked에 대한 처리를 코드상에서 디름,로, 의사선택자에 의한 스타일을 제거합니다.

--- src/components/ToggleButton/ToggleButton.view.tsx
+++ src/components/ToggleButton/ToggleButton.view.tsx
@@ -6,13 +6,6 @@ export const ToggleButtonStyled = styled.div<ToggleButtonStyledProps>`
   border-radius: ${props => props.radius ?? 5}px;
   padding: ${props => props.padding ?? 0}px;
   background: ${props => props.color ?? "#ffffff"};
-  box-shadow: ${props => props.backgroundShadow},
-    ${props => props.foregroundShadow};
+  box-shadow: ${props => props.shadow};
   transition: all 0.2s ease-in-out;
-
-  &:hover {
-    background: ${props => props.hoverColor ?? props.color};
-    box-shadow: ${props => props.hoverBackgroundShadow},
-      ${props => props.hoverForegroundShadow};
-  }
 `;
